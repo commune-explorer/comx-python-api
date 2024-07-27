5157edf292da2db3e4524d56c852fcbcf76bb870
supreme <zimulajan0@gmail.com>
2024-07-27 12:40:01 +0200

    feat: adding flake updating poetry.toml/lock

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "x";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system=system;
+          config.allowUnfree = true;
+        };
+        p2n = poetry2nix.lib.mkPoetry2Nix {
+          inherit pkgs;
+        };
+        p2n-overrides = import ./nix/poetry2nix-overrides.nix {
+          inherit pkgs p2n;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python310
+            pkgs.poetry
+            pkgs.ruff
+          ];
+        };
+        packages = rec {
+          communex = p2n.mkPoetryApplication {
+            projectDir = ./.;
+            python = pkgs.python311;
+            overrides = p2n-overrides;
+          };
+          default = communex;
+        };
+      });
+}
+
